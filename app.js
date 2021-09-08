250a21292069c3d901de59ecd3294cf370e1bdb3
koftov <ahronlu@gmail.com>
2021-09-08 23:39:44 +0300

    push

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ app.use("/api", require("./router"));
 const __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/client/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"))
   );
 }
 
